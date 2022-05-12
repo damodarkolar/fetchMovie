de5349fb730d83877979cd226faf95a73f40fd30
damodarkolar <damodarkolar@gmail.com>
2022-05-12 23:46:40 +0530

    add

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 var btn=document.querySelector("#searchForm");
-btn.addEventListener("submit", posterPreparation);
+btn.addEventListener("submit", search);
 
 
-function posterPreparation(event){
-    event.preventDefault();
+function posterPreparation(movieDetails){
+    // event.preventDefault();
     document.querySelector("#poster").innerHTML="";
-
-    var movieName=document.querySelector("#movieInput").value;
-    console.log(movieName)
     var posterDiv=document.createElement("div");
     posterDiv.setAttribute("id","posterdiv");    
-    var movieDetails=search(movieName);
-    if(movieDetails==="error"){
+    if(movieDetails.Response==="False"){
         var errimage=document.createElement("img");
         errimage.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOV2Ge2RDcQtkHTvWTdDqLwyJzHmzceKGFKw&usqp=CAU";
         errimage.setAttribute("style","width:100%")
@@ -28,24 +24,28 @@ function posterPreparation(event){
         releaseDateRatingDiv.setAttribute("id","releaseDateratingDiv");
     
         var releaseDateElement=document.createElement("p");
-        releaseDateElement.innerText=movieDetails.Year;
+        releaseDateElement.innerText="Release Year :"+movieDetails.Year;
     
         var ratingElement=document.createElement("p");
-        ratingElement.innerText=movieDetails.imdbRating;
+        ratingElement.innerText="Rating :"+movieDetails.imdbRating;
+        
     
         releaseDateRatingDiv.append(releaseDateElement,ratingElement);
-    
-        posterDiv.append(nameElement,posterUrl,releaseDateElement);
+        posterDiv.append(nameElement,posterUrl,releaseDateRatingDiv);
         document.querySelector("#poster").append(posterDiv);
     }
 }
 
-        async function search(movieName){
+        async function search(){
+            event.preventDefault();
+            var movieName=document.querySelector("#movieInput").value;
             try{
-                var result= fetch(`http://www.omdbapi.com/?t=${movieName}&apikey=3b49127f`);
+                var result=await fetch(`http://www.omdbapi.com/?t=${movieName}&apikey=3b49127f`);
                 var data=await result.json();
-                return data;
-            }catch(err){
-                return "error"
+                // console.log(data.Poster);
+                posterPreparation(data);
+
+            }catch(error){
+                posterPreparation("error");
             }
         }
